0c4c5445c334226cc0dcd97abae0a58ce813b2cd
DzNatsu <phphuong.career@gmail.com>
2024-04-28 18:58:13 +0700

    Add Package Game: create the screen

---
+++ src/Game/GameWindow.java
@@ -0,0 +1,12 @@
+package Game;
+
+import javax.swing.*;
+
+public class GameWindow {
+    public GameWindow() {
+        JFrame jframe = new JFrame("Game");
+        jframe.setSize(800, 600);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setVisible(true);
+    }
+}
