01ac422cd5e79fb7f3a5ad06cccfee8c1c3eff44
DrGnNatsu <phphuong.career@gmail.com>
2024-05-31 09:41:42 +0700

    Change the bigger map

--- src/Utilization/SupportMethods.java
+++ src/Utilization/SupportMethods.java
@@ -16,8 +16,9 @@ public class SupportMethods {
     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     //Check Solid of the level
     public static boolean isSolid(float x, float y, int[][] levelData) {
+        int maxWidth = levelData[0].length;
         //Two edge of the screen top and down
-        if (x < 0 || x >= Game.GAME_WIDTH) return true;
+        if (x < 0 || x >= maxWidth * Game.TILE_SIZE_SCALE) return true;
         if (y < 0 || y >= Game.GAME_HEIGHT) return true;
 
         float xIndex = x / Game.TILE_SIZE_SCALE;
@@ -25,6 +26,7 @@ public class SupportMethods {
 
         //Check which sprite is solid or not
         int value = levelData[(int) yIndex][(int) xIndex];
+
 
         return value != 0;
     }
